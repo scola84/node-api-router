ae0328d81c3bf12a504f53db42b397dead6687d3
M.C. van den Boogaart <scola@kvdb.name>
2016-06-23 22:03:55 +0200

    Add factory method

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-export { default as Router } from './src/router';
 export { default as errorHandler } from './src/helper/error-handler';
+
+import Router from './src/router';
+export { Router };
+
+export function router(url) {
+  return new Router(url);
+}
