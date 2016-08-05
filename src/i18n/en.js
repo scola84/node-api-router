bb544bb78d9860c5fe91120619ea5a73af7b31ef
M.C. van den Boogaart <scola@kvdb.name>
2016-08-05 12:37:11 +0200

    Add missing files

---
+++ src/i18n/en.js
@@ -0,0 +1,9 @@
+export default {
+  scola: {
+    error: {
+      invalid_method: 'This method is not allowed.',
+      invalid_path: 'The path was not found.',
+      invalid_version: 'The version was not found.'
+    }
+  }
+};
---
+++ src/i18n/nl.js
@@ -0,0 +1,9 @@
+export default {
+  scola: {
+    error: {
+      invalid_method: 'Deze bewerking is niet toegestaan.',
+      invalid_path: 'Het pad is niet gevonden.',
+      invalid_version: 'De versie is niet gevonden.'
+    }
+  }
+};
