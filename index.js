73aa052208e003d7418f93ce815f1be85b964527
M.C. van den Boogaart <scola@kvdb.name>
2017-01-23 17:10:31 +0100

    Load on app

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ import strings from './src/i18n/strings';
 export { default as Router } from './src/router';
 export { default as handleError } from './src/helper/handle-error';
 
-export function load(i18n) {
-  i18n.strings(strings);
+export function load(app) {
+  app.i18n().strings(strings);
 }
