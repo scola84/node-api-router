1b3305bd0d84c33183d421cf188e9361ac823342
M.C. van den Boogaart <scola@kvdb.name>
2017-02-04 13:32:53 +0100

    Only set strings when i18n is set

--- index.js
+++ index.js
@@ -4,5 +4,7 @@ export { default as Router } from './src/router';
 export { default as handleError } from './src/helper/handle-error';
 
 export function load(app) {
-  app.i18n().strings(strings);
+  if (app.i18n()) {
+    app.i18n().strings(strings);
+  }
 }
