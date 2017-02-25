845ae02421501f2515545fa3dc008e50018025fe
M.C. van den Boogaart <scola@kvdb.name>
2017-02-25 22:12:49 +0100

    Add short messages

--- src/i18n/en.js
+++ src/i18n/en.js
@@ -3,7 +3,12 @@ export default {
     error: {
       invalid_method: 'This method is not allowed.',
       invalid_path: 'The path was not found.',
-      invalid_version: 'The version was not found.'
+      invalid_version: 'The version was not found.',
+      short: {
+        invalid_method: 'Not Allowed',
+        invalid_path: 'Not Found',
+        invalid_version: 'Not Found'
+      }
     }
   }
 };
--- src/i18n/nl.js
+++ src/i18n/nl.js
@@ -3,7 +3,12 @@ export default {
     error: {
       invalid_method: 'Deze bewerking is niet toegestaan.',
       invalid_path: 'Het pad is niet gevonden.',
-      invalid_version: 'De versie is niet gevonden.'
+      invalid_version: 'De versie is niet gevonden.',
+      short: {
+        invalid_method: 'Niet toegestaan',
+        invalid_path: 'Niet gevonden',
+        invalid_version: 'Niet gevonden',
+      }
     }
   }
 };
