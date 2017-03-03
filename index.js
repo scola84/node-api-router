fa85e7b95306a68e9e2a4b808d91309b52800319
M.C. van den Boogaart <scola@kvdb.name>
2017-03-03 21:26:31 +0100

    Refactor exports

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
+import Router from './src/router';
+import handleError from './src/helper/handle-error';
 import strings from './src/i18n/strings';
 
-export { default as Router } from './src/router';
-export { default as handleError } from './src/helper/handle-error';
-
-export function load(app) {
+function load(app) {
   if (app.i18n()) {
     app.i18n().strings(strings);
   }
 }
+
+export {
+  Router,
+  handleError,
+  load
+};
